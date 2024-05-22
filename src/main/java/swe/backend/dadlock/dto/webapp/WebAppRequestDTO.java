bb81fae5a4ffeb491e720085f186bb7d0a90131a
strongmhk <kim6562166086@gmail.com>
2024-05-22 20:39:11 +0900

    fix: WebApp URL 생성 API 수정

--- src/main/java/swe/backend/dadlock/dto/webapp/WebAppRequestDTO.java
+++ src/main/java/swe/backend/dadlock/dto/webapp/WebAppRequestDTO.java
@@ -3,6 +3,7 @@ package swe.backend.dadlock.dto.webapp;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import swe.backend.dadlock.entity.Subject;
 import swe.backend.dadlock.entity.User;
 import swe.backend.dadlock.entity.WebApp;
 
@@ -13,12 +14,14 @@ public class WebAppRequestDTO {
     public static class CreateDTO {
         private String appUrl;
         private int timeLimit;
+        private Subject subject;
 
         public WebApp toEntity(User user) {
             return WebApp.builder()
                     .user(user)
                     .appUrl(appUrl)
                     .timeLimit(timeLimit)
+                    .subject(subject)
                     .build();
         }
     }
